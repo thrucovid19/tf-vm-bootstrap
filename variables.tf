330ee682a8bca44e8cb66bd47829bcc8ff5e826d
thrucvoid19 <thrucovid19@gmail.com>
2020-08-05 17:29:35 -0500

    second commit

--- variables.tf
+++ variables.tf
@@ -1,9 +1,36 @@
 variable "authcode" {
 }
 
-variable "panorama_bootstrap_key" {
-}
-
 variable "bootstrap_s3bucket" {
   default = "vmseries-bootstrap"
+}
+
+variable "panorama1" {
+  type = string
+  description = "Primary Panorama IP address" 
+}
+
+variable "panorama2" {
+  type = string
+  description = "Secondary Panorama IP address" 
+}
+
+variable "template" {
+  type = string
+  description = "Panorama template"
+}
+
+variable "devicegroup" {
+  type = string
+  description = "Panorama device group"
+}
+
+variable "panorama_bootstrap_key" {
+  type = string
+  description = "Panorama bootstrap key"
+}
+
+variable "hostname" {
+  type = string
+  description = "Hostname for the firewall"
 }
